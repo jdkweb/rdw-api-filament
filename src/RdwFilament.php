bd00e1e090fff0087375baffdbabe6cb0402522f
Jasper <jasper@jdkweb.nl>
2025-01-31 18:34:19 +0100

    Splitsing tussen filament en laravel

--- src/RdwFilament.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Jdkweb\Rdw\Filament;
-
-
-class RdwFilament
-{
-
-}
--- src/RdwServiceProvider.php
+++ src/RdwServiceProvider.php
@@ -9,26 +9,9 @@ class RdwServiceProvider extends ServiceProvider
 {
     final public function boot():void
     {
-//        // php artisan vendor:publish --provider="Jdkweb\Rdw\RdwServiceProvider" --tag="config"
-//        $this->publishes([
-//            dirname(__DIR__).'/config/rdw-api.php' => config_path('rdw-api.php'),
-//        ], 'config');
-//
-//        // php artisan vendor:publish --provider="Jdkweb\Rdw\RdwServiceProvider" --tag="lang"
-//        $this->publishes([
-//            dirname(__DIR__).'/lang' =>  lang_path('vendor/rdw-api'),
-//        ], 'lang');
-//
-//        // Load lang
-//        $this->loadTranslationsFrom(dirname(__DIR__).'/lang/', 'rdw-api');
-//
-//        // When not published Load config
-//        if(is_null(config('rdw-api.rdw_api_use'))) {
-//            $this->mergeConfigFrom(dirname(__DIR__).'/config/rdw-api.php', 'rdw-api');
-//        }
-
         // Demo route on and local
         if(config('rdw-api.rdw_api_demo') && env('APP_ENV') === 'local' ) {
+            // Filament demo routes
             $this->loadRoutesFrom(dirname(__DIR__).'/routes/demo.php');
             // Manually register components
             Livewire::component('rdw-api-demo', \Jdkweb\Rdw\Filament\Demo\Livewire\RdwApiDemo::class);
@@ -42,11 +25,5 @@ class RdwServiceProvider extends ServiceProvider
      */
     final public function register():void
     {
-//        $this->app->singleton(Rdw::class, function ($app) {
-//            return new Rdw();
-//        });
-//
-//        // rdw as alias for "Jdkweb\Rdw\Rdw"
-//        $this->app->alias(Rdw::class, 'rdw');
     }
 }
