106e1ea35e64c9631b4ee908381cd8f4986b108d
Freek <github@freekdk.nl>
2025-03-10 11:57:14 +0100

    import HasLabel

--- src/Enums/OutputFormat.php
+++ src/Enums/OutputFormat.php
@@ -3,9 +3,9 @@
 namespace Jdkweb\RdwApi\Filament\Enums;
 
 use Jdkweb\RdwApi\Enums\OutputFormat as baseEnum;
+use Filament\Support\Contracts\HasLabel;
 
-
-enum OutputFormat: string implements baseEnum
+enum OutputFormat: string implements baseEnum, HasLabel
 {
 
 }
