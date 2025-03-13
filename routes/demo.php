988fb27c52b6fb67fe152a2f8c75c64a17d35903
Jasper <jasper@jdkweb.nl>
2025-03-13 11:45:54 +0100

    Changes to show csrf_token()

--- routes/demo.php
+++ routes/demo.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => '/'  . config('rdw-api.rdw_api_folder') .'/' . config('rdw-api.rdw_api_filament_folder') ], function () {
-    Route::get('/'.config('rdw-api.rdw_api_demo_slug').'/change-language/{language}', function ($language) {
-        return redirect('/'.config('rdw-api.rdw_api_folder').'/'.config('rdw-api.rdw_api_filament_folder').'/'.config('rdw-api.rdw_api_demo_slug').'/'.$language);
-    });
-    Route::match(array('GET', 'POST', 'PUT'),
-        '/'.config('rdw-api.rdw_api_demo_slug')."/{language?}",
-        \Jdkweb\RdwApi\Filament\Demo\Livewire\RdwApiDemo::class)->name(config('rdw-api.rdw_api_demo_slug'));
+Route::group(['middleware' => 'web'], function () {
+    Route::group(['prefix' => '/'.config('rdw-api.rdw_api_folder').'/'.config('rdw-api.rdw_api_filament_folder')],
+        function () {
+            Route::get('/'.config('rdw-api.rdw_api_demo_slug').'/change-language/{language}', function ($language) {
+                return redirect('/'.config('rdw-api.rdw_api_folder').'/'.config('rdw-api.rdw_api_filament_folder').'/'.config('rdw-api.rdw_api_demo_slug').'/'.$language);
+            });
+            Route::match(array('GET', 'POST', 'PUT'),
+                '/'.config('rdw-api.rdw_api_demo_slug')."/{language?}",
+                \Jdkweb\RdwApi\Filament\Demo\Livewire\RdwApiDemo::class)->name(config('rdw-api.rdw_api_demo_slug'));
+        });
 });
 
--- src/Demo/views/components/layouts/rdw-api-demo.blade.php
+++ src/Demo/views/components/layouts/rdw-api-demo.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>RDW API - license plate check</title>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel=stylesheet href="https://cdn.jsdelivr.net/npm/pretty-print-json@3.0/dist/css/pretty-print-json.dark-mode.css">
